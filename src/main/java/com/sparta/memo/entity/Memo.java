0fc214ad8d2e13beaa09b7eaaabc7c31d51ab1d1
hyeonhee <nloavee@gmail.com>
2025-02-01 21:16:19 +0900

    Create, Read 구현

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,23 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Timer;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Memo {
+
+    private Long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
